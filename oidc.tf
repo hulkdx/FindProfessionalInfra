7c5c0ad7a430f002c5e2666a8eccb4e02782ca40
hulkdx <hulktroll@gmail.com>
2022-12-05 19:10:36 +0200

    oidc for vpc cni

--- oidc.tf
+++ oidc.tf
@@ -8,6 +8,33 @@
 #   url             = data.tls_certificate.eks.url
 # }
 
+# data "aws_iam_policy_document" "oidc" {
+#   statement {
+#     actions = ["sts:AssumeRoleWithWebIdentity"]
+#     effect  = "Allow"
+
+#     condition {
+#       test     = "StringEquals"
+#       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+#       values   = ["system:serviceaccount:kube-system:aws-node"]
+#     }
+
+#     principals {
+#       identifiers = [aws_iam_openid_connect_provider.eks.arn]
+#       type        = "Federated"
+#     }
+#   }
+# }
+
+# resource "aws_iam_role" "oidc" {
+#   assume_role_policy = data.aws_iam_policy_document.oidc.json
+# }
+
+# resource "aws_iam_role_policy_attachment" "oidc" {
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+#   role       = aws_iam_role.oidc.name
+# }
+
 # output "oidc_arn" {
-#   value = aws_iam_role.test_oidc.arn
+#   value = aws_iam_openid_connect_provider.eks.arn
 # }
