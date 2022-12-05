74254845459edb375c7b17c5d17ee258ecd8a7a1
hulkdx <hulktroll@gmail.com>
2022-12-05 19:11:33 +0200

    vpc cni added

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@
 #   ]
 
 #   capacity_type  = "ON_DEMAND"
-#   instance_types = ["t3.small"]
+#   instance_types = ["t3.micro"]
 
 #   scaling_config {
 #     desired_size = 1
@@ -54,6 +54,39 @@
 #     aws_eks_cluster.main_eks.vpc_config[0].cluster_security_group_id,
 #     aws_security_group.node_group.id,
 #   ]
+
+#   metadata_options {
+#     http_endpoint               = "enabled"
+#     http_protocol_ipv6          = "disabled"
+#     http_put_response_hop_limit = 2
+#     http_tokens                 = "required"
+#     instance_metadata_tags      = "disabled"
+#   }
+#   user_data = data.cloudinit_config.linux_eks_managed_node_group.rendered
+
+#   depends_on = [
+#     aws_eks_addon.vpc_cni,
+#   ]
+# }
+
+# # NOTE: User data must in MIME multi-part archive format
+# data "cloudinit_config" "linux_eks_managed_node_group" {
+#   base64_encode = true
+#   gzip          = false
+#   boundary      = "//"
+
+#   part {
+#     content_type = "text/x-shellscript"
+#     content      = <<-EOF
+#       !/bin/bash
+#       set -o xtrace
+#       /etc/eks/bootstrap.sh ${var.cluster_name} \
+#       --b64-cluster-ca ${aws_eks_cluster.main_eks.certificate_authority[0].data}
+#       --apiserver-endpoint ${aws_eks_cluster.main_eks.endpoint}
+#       --use-max-pods false \
+#       --kubelet-extra-args '--max-pods=34'
+#     EOF
+#   }
 # }
 
 # resource "aws_security_group" "node_group" {
@@ -64,5 +97,46 @@
 #     to_port     = 30000
 #     protocol    = "tcp"
 #     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_eks_addon" "vpc_cni" {
+#   cluster_name = aws_eks_cluster.main_eks.name
+#   addon_name   = "vpc-cni"
+#   resolve_conflicts = "OVERWRITE"
+#   addon_version = data.aws_eks_addon_version.latest.version
+#   service_account_role_arn = aws_iam_role.oidc.arn
+
+#   depends_on = [
+#     # Modify VPC CNI ahead of addons
+#     null_resource.kubectl_set_env
+#   ]
+# }
+
+# data "aws_eks_addon_version" "latest" {
+#   addon_name         = "vpc-cni"
+#   kubernetes_version = aws_eks_cluster.main_eks.version
+#   most_recent        = true
+# }
+
+
+# ##
+# ##
+# ##
+
+# resource "null_resource" "kubectl_set_env" {
+#   triggers = {
+#     once = true
+#   }
+
+#   provisioner "local-exec" {
+#     interpreter = ["/bin/bash", "-c"]
+
+#     # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+#     command = <<-EOT
+#       aws eks update-kubeconfig --name findprofessional_cluster
+#       kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
+#       kubectl set env daemonset aws-node -n kube-system WARM_PREFIX_TARGET=1
+#     EOT
 #   }
 # }
