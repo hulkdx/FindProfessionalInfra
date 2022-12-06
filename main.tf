6b96f686624d506666f3d8df3e7c51f477975191
hulkdx <hulktroll@gmail.com>
2022-12-06 15:44:48 +0200

    enable eks

--- main.tf
+++ main.tf
@@ -1,115 +1,115 @@
-# resource "aws_eks_cluster" "main_eks" {
-#   name     = var.cluster_name
-#   role_arn = aws_iam_role.eks_role.arn
-#   vpc_config {
-#     subnet_ids = local.public_subnet_ids
-#   }
-#   depends_on = [aws_iam_role_policy_attachment.eks_policy_AmazonEKSClusterPolicy]
-# }
+ resource "aws_eks_cluster" "main_eks" {
+   name     = var.cluster_name
+   role_arn = aws_iam_role.eks_role.arn
+   vpc_config {
+     subnet_ids = local.public_subnet_ids
+   }
+   depends_on = [aws_iam_role_policy_attachment.eks_policy_AmazonEKSClusterPolicy]
+ }
 
-# resource "aws_eks_node_group" "main_eks_node_group" {
-#   cluster_name   = aws_eks_cluster.main_eks.name
-#   node_role_arn  = aws_iam_role.eks_node_group_role.arn
-#   subnet_ids     = local.public_subnet_ids
-#   capacity_type  = "ON_DEMAND"
-#   instance_types = ["t3.micro"]
-#   scaling_config {
-#     desired_size = 1
-#     max_size     = 1
-#     min_size     = 0
-#   }
-#   update_config {
-#     max_unavailable = 1
-#   }
-#   launch_template {
-#     name    = aws_launch_template.node_group.name
-#     version = aws_launch_template.node_group.latest_version
-#   }
-#   depends_on = [
-#     aws_iam_role_policy_attachment.eks_node_policy_AmazonEKSWorkerNodePolicy,
-#     aws_iam_role_policy_attachment.eks_node_policy_AmazonEC2ContainerRegistryReadOnly,
-#     aws_iam_role_policy_attachment.eks_node_policy_AmazonEKS_CNI_Policy,
-#   ]
-# }
+ resource "aws_eks_node_group" "main_eks_node_group" {
+   cluster_name   = aws_eks_cluster.main_eks.name
+   node_role_arn  = aws_iam_role.eks_node_group_role.arn
+   subnet_ids     = local.public_subnet_ids
+   capacity_type  = "ON_DEMAND"
+   instance_types = ["t3.micro"]
+   scaling_config {
+     desired_size = 1
+     max_size     = 1
+     min_size     = 0
+   }
+   update_config {
+     max_unavailable = 1
+   }
+   launch_template {
+     name    = aws_launch_template.node_group.name
+     version = aws_launch_template.node_group.latest_version
+   }
+   depends_on = [
+     aws_iam_role_policy_attachment.eks_node_policy_AmazonEKSWorkerNodePolicy,
+     aws_iam_role_policy_attachment.eks_node_policy_AmazonEC2ContainerRegistryReadOnly,
+     aws_iam_role_policy_attachment.eks_node_policy_AmazonEKS_CNI_Policy,
+   ]
+ }
 
-# resource "aws_security_group" "node_group" {
-#   vpc_id = aws_vpc.main_vpc.id
-#   ingress {
-#     from_port   = 30000
-#     to_port     = 30000
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+ resource "aws_security_group" "node_group" {
+   vpc_id = aws_vpc.main_vpc.id
+   ingress {
+     from_port   = 30000
+     to_port     = 30000
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+ }
 
-# resource "aws_launch_template" "node_group" {
-#   vpc_security_group_ids = [
-#     aws_eks_cluster.main_eks.vpc_config[0].cluster_security_group_id,
-#     aws_security_group.node_group.id,
-#   ]
-#   metadata_options {
-#     http_put_response_hop_limit = 2
-#     http_tokens                 = "required"
-#   }
-#   user_data  = data.cloudinit_config.node_group.rendered
-#   depends_on = [aws_eks_addon.vpc_cni]
-# }
+ resource "aws_launch_template" "node_group" {
+   vpc_security_group_ids = [
+     aws_eks_cluster.main_eks.vpc_config[0].cluster_security_group_id,
+     aws_security_group.node_group.id,
+   ]
+   metadata_options {
+     http_put_response_hop_limit = 2
+     http_tokens                 = "required"
+   }
+   user_data  = data.cloudinit_config.node_group.rendered
+   depends_on = [aws_eks_addon.vpc_cni]
+ }
 
-# data "cloudinit_config" "node_group" {
-#   base64_encode = true
-#   gzip          = false
-#   boundary      = "//"
-#   part {
-#     content_type = "text/x-shellscript"
-#     content      = local.increase_max_pods_script
-#   }
-# }
+ data "cloudinit_config" "node_group" {
+   base64_encode = true
+   gzip          = false
+   boundary      = "//"
+   part {
+     content_type = "text/x-shellscript"
+     content      = local.increase_max_pods_script
+   }
+ }
 
-# # -------------------------------------------------------------------------------------
-# # Increase max pods
-# #	https://aws.amazon.com/blogs/containers/amazon-vpc-cni-increases-pods-per-node-limits/
-# # https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
-# # -------------------------------------------------------------------------------------
-# locals {
-#   # t3.micro
-#   max_pods = "32"
-# }
-# # step 1
-# resource "null_resource" "kubectl_set_env" {
-#   triggers = {
-#     once = true
-#   }
-#   provisioner "local-exec" {
-#     interpreter = ["/bin/bash", "-c"]
-#     command     = <<-EOT
-#       aws eks update-kubeconfig --name ${aws_eks_cluster.main_eks.name}
-#       kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
-#     EOT
-#   }
-# }
-# # step 2
-# resource "aws_eks_addon" "vpc_cni" {
-#   cluster_name             = aws_eks_cluster.main_eks.name
-#   addon_name               = "vpc-cni"
-#   resolve_conflicts        = "OVERWRITE"
-#   addon_version            = data.aws_eks_addon_version.latest.version
-#   service_account_role_arn = aws_iam_role.oidc.arn
-#   depends_on               = [null_resource.kubectl_set_env]
-# }
-# data "aws_eks_addon_version" "latest" {
-#   addon_name         = "vpc-cni"
-#   kubernetes_version = aws_eks_cluster.main_eks.version
-#   most_recent        = true
-# }
-# # step 3
-# locals {
-#   increase_max_pods_script = <<-EOF
-#     !/bin/bash
-#     set -o xtrace
-#     /etc/eks/bootstrap.sh ${var.cluster_name} \
-#       --b64-cluster-ca ${aws_eks_cluster.main_eks.certificate_authority[0].data}
-#       --apiserver-endpoint ${aws_eks_cluster.main_eks.endpoint}
-#       --use-max-pods false \
-#       --kubelet-extra-args '--max-pods=34'
-#   EOF
-# }
+ # -------------------------------------------------------------------------------------
+ # Increase max pods
+ #	https://aws.amazon.com/blogs/containers/amazon-vpc-cni-increases-pods-per-node-limits/
+ # https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+ # -------------------------------------------------------------------------------------
+ locals {
+   # t3.micro
+   max_pods = "32"
+ }
+ # step 1
+ resource "null_resource" "kubectl_set_env" {
+   triggers = {
+     once = true
+   }
+   provisioner "local-exec" {
+     interpreter = ["/bin/bash", "-c"]
+     command     = <<-EOT
+       aws eks update-kubeconfig --name ${aws_eks_cluster.main_eks.name}
+       kubectl set env daemonset aws-node -n kube-system ENABLE_PREFIX_DELEGATION=true
+     EOT
+   }
+ }
+ # step 2
+ resource "aws_eks_addon" "vpc_cni" {
+   cluster_name             = aws_eks_cluster.main_eks.name
+   addon_name               = "vpc-cni"
+   resolve_conflicts        = "OVERWRITE"
+   addon_version            = data.aws_eks_addon_version.latest.version
+   service_account_role_arn = aws_iam_role.oidc.arn
+   depends_on               = [null_resource.kubectl_set_env]
+ }
+ data "aws_eks_addon_version" "latest" {
+   addon_name         = "vpc-cni"
+   kubernetes_version = aws_eks_cluster.main_eks.version
+   most_recent        = true
+ }
+ # step 3
+ locals {
+   increase_max_pods_script = <<-EOF
+     !/bin/bash
+     set -o xtrace
+     /etc/eks/bootstrap.sh ${var.cluster_name} \
+       --b64-cluster-ca ${aws_eks_cluster.main_eks.certificate_authority[0].data}
+       --apiserver-endpoint ${aws_eks_cluster.main_eks.endpoint}
+       --use-max-pods false \
+       --kubelet-extra-args '--max-pods=34'
+   EOF
+ }
--- oidc.tf
+++ oidc.tf
@@ -1,40 +1,40 @@
-# data "tls_certificate" "eks" {
-#   url = aws_eks_cluster.main_eks.identity[0].oidc[0].issuer
-# }
+ data "tls_certificate" "eks" {
+   url = aws_eks_cluster.main_eks.identity[0].oidc[0].issuer
+ }
 
-# resource "aws_iam_openid_connect_provider" "eks" {
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = data.tls_certificate.eks.certificates[*].sha1_fingerprint
-#   url             = data.tls_certificate.eks.url
-# }
+ resource "aws_iam_openid_connect_provider" "eks" {
+   client_id_list  = ["sts.amazonaws.com"]
+   thumbprint_list = data.tls_certificate.eks.certificates[*].sha1_fingerprint
+   url             = data.tls_certificate.eks.url
+ }
 
-# data "aws_iam_policy_document" "oidc" {
-#   statement {
-#     actions = ["sts:AssumeRoleWithWebIdentity"]
-#     effect  = "Allow"
+ data "aws_iam_policy_document" "oidc" {
+   statement {
+     actions = ["sts:AssumeRoleWithWebIdentity"]
+     effect  = "Allow"
 
-#     condition {
-#       test     = "StringEquals"
-#       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-#       values   = ["system:serviceaccount:kube-system:aws-node"]
-#     }
+     condition {
+       test     = "StringEquals"
+       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+       values   = ["system:serviceaccount:kube-system:aws-node"]
+     }
 
-#     principals {
-#       identifiers = [aws_iam_openid_connect_provider.eks.arn]
-#       type        = "Federated"
-#     }
-#   }
-# }
+     principals {
+       identifiers = [aws_iam_openid_connect_provider.eks.arn]
+       type        = "Federated"
+     }
+   }
+ }
 
-# resource "aws_iam_role" "oidc" {
-#   assume_role_policy = data.aws_iam_policy_document.oidc.json
-# }
+ resource "aws_iam_role" "oidc" {
+   assume_role_policy = data.aws_iam_policy_document.oidc.json
+ }
 
-# resource "aws_iam_role_policy_attachment" "oidc" {
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-#   role       = aws_iam_role.oidc.name
-# }
+ resource "aws_iam_role_policy_attachment" "oidc" {
+   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+   role       = aws_iam_role.oidc.name
+ }
 
-# output "oidc_arn" {
-#   value = aws_iam_openid_connect_provider.eks.arn
-# }
+ output "oidc_arn" {
+   value = aws_iam_openid_connect_provider.eks.arn
+ }
