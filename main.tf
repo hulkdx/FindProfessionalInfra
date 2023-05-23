9a3af6ee56b81ce85a74f1d81e19618a62806bc0
hulkdx <hulktroll@gmail.com>
2023-05-23 18:19:02 +0300

    Fixed an issue with coredns

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@
 #   }
 #   depends_on = [aws_iam_role_policy_attachment.eks_policy_AmazonEKSClusterPolicy]
 # }
-
+#
 # resource "aws_eks_node_group" "main_eks_node_group" {
 #   cluster_name   = aws_eks_cluster.main_eks.name
 #   node_role_arn  = aws_iam_role.eks_node_group_role.arn
@@ -31,7 +31,7 @@
 #     aws_iam_role_policy_attachment.eks_node_policy_AmazonEKS_CNI_Policy,
 #   ]
 # }
-
+#
 # resource "aws_security_group" "node_group" {
 #   vpc_id = aws_vpc.main_vpc.id
 #   ingress {
@@ -41,14 +41,14 @@
 #     cidr_blocks = ["0.0.0.0/0"]
 #   }
 # }
-
+#
 # resource "aws_launch_template" "node_group" {
 #   vpc_security_group_ids = [
 #     aws_eks_cluster.main_eks.vpc_config[0].cluster_security_group_id,
 #     aws_security_group.node_group.id,
 #   ]
 # }
-
+#
 # resource "aws_eks_addon" "vpc_cni" {
 #   cluster_name             = aws_eks_cluster.main_eks.name
 #   addon_name               = "vpc-cni"
@@ -64,16 +64,17 @@
 #   })
 # }
 # data "aws_eks_addon_version" "latest" {
-#   addon_name         = "vpc-cni"
-#   kubernetes_version = aws_eks_cluster.main_eks.version
-#   most_recent        = true
+#    addon_name         = "vpc-cni"
+#    kubernetes_version = aws_eks_cluster.main_eks.version
+#    most_recent        = true
 # }
 #
 # resource "aws_eks_addon" "coredns" {
-#   cluster_name             = aws_eks_cluster.main_eks.name
-#   addon_name               = "coredns"
+#    cluster_name             = aws_eks_cluster.main_eks.name
+#    addon_name               = "coredns"
+#    depends_on     = [aws_eks_node_group.main_eks_node_group]
 # }
 # resource "aws_eks_addon" "kube-proxy" {
-#   cluster_name             = aws_eks_cluster.main_eks.name
-#   addon_name               = "kube-proxy"
+#    cluster_name             = aws_eks_cluster.main_eks.name
+#    addon_name               = "kube-proxy"
 # }
